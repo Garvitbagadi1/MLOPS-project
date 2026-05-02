3f39fba3e9b36431c6d0330eca77e69bc1cc2166
Garvit Bagadi <garvitbagadi0851@gmail.com>
2026-05-02 16:56:24 +0530

    data validation added

---
+++ src/myproject/components/data_validation.py
@@ -0,0 +1,33 @@
+import os
+from myproject import logger
+from myproject.entity.config_entity import DataValidationConfig
+import pandas as pd
+
+class DataValidation:
+    def __init__(self, config):
+        self.config = config
+
+    def validation_all_columns(self) -> bool:
+        try:
+            print("Reading file:", self.config.unzip_data_dir)
+            print("Exists:", os.path.exists(self.config.unzip_data_dir))
+
+            data = pd.read_csv(self.config.unzip_data_dir)
+
+            all_cols = list(data.columns)
+            all_schema = self.config.all_schema.keys()
+
+            validation_status = True
+
+            for col in all_cols:
+                if col not in all_schema:
+                    validation_status = False
+                    break
+
+            with open(self.config.STATUS_FILE, 'w') as f:
+                f.write(f"Validation status: {validation_status}")
+
+            return validation_status
+
+        except Exception as e:
+            raise e
